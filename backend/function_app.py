1def7d4184db84d3a7ca1837bd1c18d7cd4ac4ca
Nagarjuna Tella <nagarjunatella@hotmail.com>
2025-03-27 20:50:35 -0400

    [FIX] Bug fixes

--- backend/function_app.py
+++ backend/function_app.py
@@ -29,8 +29,7 @@ client = CosmosClient(COSMOS_ENDPOINT, COSMOS_KEY)
 database = client.create_database_if_not_exists(id=DATABASE_NAME)
 container = database.create_container_if_not_exists(
     id=CONTAINER_NAME,
-    partition_key=PartitionKey(path="/id"),
-    offer_throughput=400
+    partition_key=PartitionKey(path="/id")
 )
 
 class CosmosDBStore:
